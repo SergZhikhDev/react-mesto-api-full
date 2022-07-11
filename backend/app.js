422a3e2bab4877bcdd8fb5ee82b96e9c1c434ac2
Sergey Zhikharev <as1234531@yandex.ru>
2022-07-11 19:38:50 +0400

    3000

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-// mongoose.connect('mongodb://localhost:3000/mestodb');
+// mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://localhost:3000/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
