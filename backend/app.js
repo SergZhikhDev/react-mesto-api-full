76f1bac1078bc3f1b73d538e798215b9511bcf8e
Sergey Zhikharev <as1234531@yandex.ru>
2022-07-17 08:53:56 +0400

    Изменил baseURI  в папке frontend

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+// mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 mongoose.connect('mongodb://localhost:3000/mestodb');
 
 app.use(bodyParser.json());
